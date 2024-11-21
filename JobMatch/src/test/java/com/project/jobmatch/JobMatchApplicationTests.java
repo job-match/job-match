5e2a89118aae29c3b4d05388e4f3384bfc86b8c2
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-21 13:54:32 +0200

    init project

---
+++ JobMatch/src/test/java/com/project/jobmatch/JobMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
