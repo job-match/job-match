0e30e7c9da4eb43f17768c5fe9e97ceed5c3c11d
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-24 14:54:31 +0200

    small fix related to @Service and @Repository

--- JobMatch/src/main/java/com/project/jobmatch/repositories/ProfessionalRepositoryImpl.java
+++ JobMatch/src/main/java/com/project/jobmatch/repositories/ProfessionalRepositoryImpl.java
@@ -1,4 +1,7 @@
 package com.project.jobmatch.repositories;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProfessionalRepositoryImpl {
 }
--- JobMatch/src/main/java/com/project/jobmatch/services/ProfessionalServiceImpl.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/ProfessionalServiceImpl.java
@@ -1,4 +1,7 @@
 package com.project.jobmatch.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProfessionalServiceImpl {
 }
