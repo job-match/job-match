70cc516a78ca1b92655192e697a9eda62dac7622
Owner <kirilovmail90@gmail.com>
2024-11-25 12:57:44 +0200

    started implementing Company.java

--- JobMatch/src/main/java/com/project/jobmatch/models/Company.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/Company.java
@@ -1,4 +1,15 @@
 package com.project.jobmatch.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "companies")
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
+    private int id;
+
+
 }
