fbf6b555f5f3eec386d86b0d644770dea0756e8b
annavulcheva <anna.valcheva26@gmail.com>
2024-11-25 12:38:04 +0200

    implemented Skill entity

--- JobMatch/src/main/java/com/project/jobmatch/models/Skill.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/Skill.java
@@ -1,4 +1,50 @@
 package com.project.jobmatch.models;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "skills")
 public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "skill_id")
+    private int id;
+
+    @Column(name = "type")
+    private String type;
+
+    public Skill() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Skill skill = (Skill) o;
+        return id == skill.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
