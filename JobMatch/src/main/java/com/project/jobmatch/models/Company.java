2eccc0bc7c16e07c0b3b05b34e9781ecde767675
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 12:03:55 +0200

    added getters & setters for username, pass & email in Company & Professional

--- JobMatch/src/main/java/com/project/jobmatch/models/Company.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/Company.java
@@ -56,6 +56,38 @@ public class Company {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getDescription() {
         return description;
     }
