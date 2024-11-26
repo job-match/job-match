b80bf7e08e68ad7e9fee5c500520ce162e2bbce9
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 12:35:19 +0200

    assigned url endpoints for REST controllers

--- JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/ProfessionalRestController.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/ProfessionalRestController.java
@@ -1,4 +1,9 @@
 package com.project.jobmatch.controllers.rest.company;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/company-portal/professionals")
 public class ProfessionalRestController {
 }
--- JobMatch/src/main/java/com/project/jobmatch/controllers/rest/professional/ProfessionalRestController.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/rest/professional/ProfessionalRestController.java
@@ -1,4 +1,9 @@
 package com.project.jobmatch.controllers.rest.professional;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/professional-portal/professionals")
 public class ProfessionalRestController {
 }
