e8f6c1fe7fcfa9eed90f3f3f51e2cbae90c80ada
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 16:35:30 +0200

    created EntityDuplicateException

---
+++ JobMatch/src/main/java/com/project/jobmatch/exceptions/EntityDuplicateException.java
@@ -0,0 +1,11 @@
+package com.project.jobmatch.exceptions;
+
+public class EntityDuplicateException extends RuntimeException {
+    public EntityDuplicateException(String type, int id) {
+        this(type, "id", String.valueOf(id));
+    }
+
+    public EntityDuplicateException(String type, String attribute, String value) {
+        super(String.format("%s with %s %s already exists.", type, attribute, value));
+    }
+}
