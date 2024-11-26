5f2a09c4add149f81854bc9a7a94a190ef9f0c70
annavulcheva <anna.valcheva26@gmail.com>
2024-11-26 16:45:58 +0200

    implemented JabAdDtoIn, in process of implementing its methods in the ModelMapper class

--- JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
+++ JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
@@ -3,6 +3,7 @@ package com.project.jobmatch.helpers;
 import com.project.jobmatch.models.JobAd;
 import com.project.jobmatch.models.dto.JobAdDtoIn;
 import com.project.jobmatch.models.dto.JobAdDtoOut;
+import com.project.jobmatch.services.interfaces.JobAdService;
 import com.project.jobmatch.services.interfaces.ProfessionalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,10 +15,23 @@ import java.util.stream.Collectors;
 @Component
 public class ModelMapper {
     private final ProfessionalService professionalService;
+    private final JobAdService jobAdService;
 
     @Autowired
-    public ModelMapper(ProfessionalService profService) {
+    public ModelMapper(ProfessionalService profService,
+                       JobAdService jobAdService) {
         this.professionalService = profService;
+        this.jobAdService = jobAdService;
+    }
+
+    public JobAd fromJobAdDtoIn(JobAdDtoIn jobAdDtoIn) {
+        JobAd jobAd = new JobAd();
+        jobAd.setPositionTitle(jobAdDtoIn.getTitle());
+        jobAd.setMinSalaryBoundary(jobAdDtoIn.getMinSalaryBoundary());
+        jobAd.setMaxSalaryBoundary(jobAdDtoIn.getMaxSalaryBoundary());
+        jobAd.setJobDescription(jobAdDtoIn.getDescription());
+    //    jobAd.setLocation(jobAdDtoIn.getLocation());
+        return jobAd;
     }
 
     public JobAdDtoOut fromJobAdToJobAdDtoOut(JobAd jobAd) {
--- JobMatch/src/main/java/com/project/jobmatch/models/dto/JobAdDtoIn.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/dto/JobAdDtoIn.java
@@ -1,4 +1,82 @@
 package com.project.jobmatch.models.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
 public class JobAdDtoIn {
+
+    @NotNull(message = "Title cannot be empty!")
+    @Size(min = 2, max = 50, message = "Title should be up to 50 symbols!")
+    private String title;
+
+    @NotNull(message = "Min Salary cannot be empty!")
+    @Size(min = 1, message = "Min Salary cannot be a negative number!")
+    private double minSalaryBoundary;
+
+    @NotNull(message = "Max Salary cannot be empty!")
+    @Size(min = 1, message = "Max Salary cannot be a negative number!")
+    private double maxSalaryBoundary;
+
+    @NotNull(message = "Job description cannot be empty!")
+    @Size(min = 2, max = 1000, message = "Description should be up to 1000 symbols!")
+    private String description;
+
+    @NotNull(message = "Location cannot be empty!")
+    @Size(min = 2, max = 50, message = "Location should be up to 50 symbols")
+    private String location;
+
+    private Set<String> requirements;
+
+    public JobAdDtoIn() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getMinSalaryBoundary() {
+        return minSalaryBoundary;
+    }
+
+    public void setMinSalaryBoundary(double minSalaryBoundary) {
+        this.minSalaryBoundary = minSalaryBoundary;
+    }
+
+    public double getMaxSalaryBoundary() {
+        return maxSalaryBoundary;
+    }
+
+    public void setMaxSalaryBoundary(double maxSalaryBoundary) {
+        this.maxSalaryBoundary = maxSalaryBoundary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Set<String> getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(Set<String> requirements) {
+        this.requirements = requirements;
+    }
 }
