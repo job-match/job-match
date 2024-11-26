14d8f16caa67a40492b8225cdeb515f3fc0b9c48
annavulcheva <anna.valcheva26@gmail.com>
2024-11-26 16:12:43 +0200

    - Implemented JobAdDtoOut and its model mapper methods

--- JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
+++ JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
@@ -1,8 +1,15 @@
 package com.project.jobmatch.helpers;
 
+import com.project.jobmatch.models.JobAd;
+import com.project.jobmatch.models.dto.JobAdDtoIn;
+import com.project.jobmatch.models.dto.JobAdDtoOut;
 import com.project.jobmatch.services.interfaces.ProfessionalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ModelMapper {
@@ -11,5 +18,27 @@ public class ModelMapper {
     @Autowired
     public ModelMapper(ProfessionalService profService) {
         this.professionalService = profService;
+    }
+
+    public JobAdDtoOut fromJobAdToJobAdDtoOut(JobAd jobAd) {
+        JobAdDtoOut jobAdDtoOut = new JobAdDtoOut();
+        jobAdDtoOut.setTitle(jobAd.getPositionTitle());
+        jobAdDtoOut.setDescription(jobAd.getJobDescription());
+        jobAdDtoOut.setMinSalaryBoundary(jobAd.getMinSalaryBoundary());
+        jobAdDtoOut.setMaxSalaryBoundary(jobAd.getMaxSalaryBoundary());
+        jobAdDtoOut.setLocation(jobAd.getLocation().getName());
+        jobAdDtoOut.setStatus(jobAd.getStatus().getType());
+
+        return jobAdDtoOut;
+    }
+
+    public List<JobAdDtoOut> fromJobAdToJobAdDtoOutList(List<JobAd> jobAds) {
+        if(jobAds == null) {
+            return new ArrayList<>();
+        }
+
+        return jobAds.stream()
+                .map(this::fromJobAdToJobAdDtoOut)
+                .collect(Collectors.toList());
     }
 }
--- JobMatch/src/main/java/com/project/jobmatch/models/dto/JobAdDtoOut.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/dto/JobAdDtoOut.java
@@ -1,4 +1,62 @@
 package com.project.jobmatch.models.dto;
 
 public class JobAdDtoOut {
+
+    private String title;
+    private double minSalaryBoundary;
+    private double maxSalaryBoundary;
+    private String description;
+    private String location;
+    private String status;
+
+    public JobAdDtoOut() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getMinSalaryBoundary() {
+        return minSalaryBoundary;
+    }
+
+    public void setMinSalaryBoundary(double minSalaryBoundary) {
+        this.minSalaryBoundary = minSalaryBoundary;
+    }
+
+    public double getMaxSalaryBoundary() {
+        return maxSalaryBoundary;
+    }
+
+    public void setMaxSalaryBoundary(double maxSalaryBoundary) {
+        this.maxSalaryBoundary = maxSalaryBoundary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
