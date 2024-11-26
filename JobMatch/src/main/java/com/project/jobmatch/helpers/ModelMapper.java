357fce921a94137108c9e0f57bf2253db1b355be
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 19:43:17 +0200

    impl Professional DTOs in ModelMapper

--- JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
+++ JobMatch/src/main/java/com/project/jobmatch/helpers/ModelMapper.java
@@ -1,15 +1,86 @@
 package com.project.jobmatch.helpers;
 
+import com.project.jobmatch.models.Professional;
+import com.project.jobmatch.models.dto.ProfessionalDtoInCreate;
+import com.project.jobmatch.models.dto.ProfessionalDtoInUpdate;
+import com.project.jobmatch.models.dto.ProfessionalDtoOut;
+import com.project.jobmatch.services.interfaces.LocationService;
 import com.project.jobmatch.services.interfaces.ProfessionalService;
+import com.project.jobmatch.services.interfaces.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ModelMapper {
     private final ProfessionalService professionalService;
+    private final LocationService locationService;
+    private final StatusService statusService;
 
     @Autowired
-    public ModelMapper(ProfessionalService profService) {
-        this.professionalService = profService;
+    public ModelMapper(ProfessionalService professionalService,
+                       LocationService locationService,
+                       StatusService statusService) {
+        this.professionalService = professionalService;
+        this.locationService = locationService;
+        this.statusService = statusService;
+    }
+
+    public ProfessionalDtoOut fromProfessionalToProfessionalDtoOut(Professional professional) {
+        ProfessionalDtoOut professionalDtoOut = new ProfessionalDtoOut();
+
+        professionalDtoOut.setUsername(professional.getUsername());
+        professionalDtoOut.setFirstName(professional.getFirstName());
+        professionalDtoOut.setLastName(professional.getLastName());
+        professionalDtoOut.setEmail(professional.getEmail());
+        professionalDtoOut.setSummary(professional.getSummary());
+        professionalDtoOut.setLocation(professional.getLocation().getName());
+        professionalDtoOut.setStatus(professional.getStatus().getType());
+
+        return professionalDtoOut;
+    }
+
+    public Professional fromProfessionalDtoInToProfessional(ProfessionalDtoInCreate professionalDtoInCreateIn) {
+        Professional professional = new Professional();
+
+        professional.setUsername(professionalDtoInCreateIn.getUsername());
+        professional.setPassword(professionalDtoInCreateIn.getPassword());
+        professional.setFirstName(professionalDtoInCreateIn.getFirstName());
+        professional.setLastName(professionalDtoInCreateIn.getLastName());
+        professional.setEmail(professionalDtoInCreateIn.getEmail());
+        professional.setSummary(professionalDtoInCreateIn.getSummary());
+        professional.setLocation(locationService.getLocationByName(professionalDtoInCreateIn.getLocation()));
+        professional.setStatus(statusService.getStatusByType("Active"));
+
+        return professional;
+    }
+
+    public Professional fromProfessionalDtoInToProfessional(int id, ProfessionalDtoInUpdate professionalDtoInUpdate) {
+        Professional professional = new Professional();
+        professional.setId(id);
+
+        professional.setUsername(professionalService.getProfessionalById(id).getUsername());
+        professional.setPassword(professionalDtoInUpdate.getPassword());
+        professional.setFirstName(professionalDtoInUpdate.getFirstName());
+        professional.setLastName(professionalDtoInUpdate.getLastName());
+        professional.setEmail(professionalDtoInUpdate.getEmail());
+        professional.setSummary(professionalDtoInUpdate.getSummary());
+        professional.setLocation(locationService.getLocationByName(professionalDtoInUpdate.getLocation()));
+        professional.setStatus(statusService.getStatusByType(professionalDtoInUpdate.getStatus()));
+
+        return professional;
+    }
+
+    public List<ProfessionalDtoOut> fromListProfessionalsToListProfessionalDtoOut(List<Professional> professionals) {
+        if (professionals == null) {
+            return new ArrayList<>();
+        }
+
+        return professionals.stream()
+                .map(this::fromProfessionalToProfessionalDtoOut)
+                .collect(Collectors.toList());
     }
 }
