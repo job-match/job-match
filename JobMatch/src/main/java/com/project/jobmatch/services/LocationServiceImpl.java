66cda9436611b2876bf4c89cdfc076dc94c65cbb
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 18:02:42 +0200

    updated annotations in LocationService & StatusService and their impl

--- JobMatch/src/main/java/com/project/jobmatch/services/LocationServiceImpl.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/LocationServiceImpl.java
@@ -4,6 +4,7 @@ import com.project.jobmatch.exceptions.EntityNotFoundException;
 import com.project.jobmatch.models.Location;
 import com.project.jobmatch.repositories.interfaces.LocationRepository;
 import com.project.jobmatch.services.interfaces.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class LocationServiceImpl implements LocationService {
 
     private final LocationRepository locationRepository;
 
+    @Autowired
     public LocationServiceImpl(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
     }
--- JobMatch/src/main/java/com/project/jobmatch/services/StatusServiceImpl.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/StatusServiceImpl.java
@@ -4,11 +4,15 @@ import com.project.jobmatch.exceptions.EntityNotFoundException;
 import com.project.jobmatch.models.Status;
 import com.project.jobmatch.repositories.interfaces.StatusRepository;
 import com.project.jobmatch.services.interfaces.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StatusServiceImpl implements StatusService {
 
     private final StatusRepository statusRepository;
 
+    @Autowired
     public StatusServiceImpl(StatusRepository statusRepository) {
         this.statusRepository = statusRepository;
     }
