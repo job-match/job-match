8a3fe5c2e7189a178e3273cbf082218b7da1aac1
annavulcheva <anna.valcheva26@gmail.com>
2024-11-26 16:13:20 +0200

    - changed the return type in the getAllJobAds and getJobAdById methods

--- JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobAdForCompaniesRestController.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobAdForCompaniesRestController.java
@@ -3,7 +3,9 @@ package com.project.jobmatch.controllers.rest.company;
 import com.project.jobmatch.exceptions.AuthorizationException;
 import com.project.jobmatch.exceptions.EntityNotFoundException;
 import com.project.jobmatch.helpers.AuthenticationHelper;
+import com.project.jobmatch.helpers.ModelMapper;
 import com.project.jobmatch.models.JobAd;
+import com.project.jobmatch.models.dto.JobAdDtoOut;
 import com.project.jobmatch.services.interfaces.CompanyService;
 import com.project.jobmatch.services.interfaces.JobAdService;
 import org.springframework.http.HttpHeaders;
@@ -20,33 +22,35 @@ public class JobAdForCompaniesRestController {
     private final JobAdService jobAdService;
     private final CompanyService companyService;
     private final AuthenticationHelper authenticationHelper;
+    private final ModelMapper modelMapper;
 
-    public JobAdForCompaniesRestController(JobAdService jobAdService, CompanyService companyService, AuthenticationHelper authenticationHelper) {
+    public JobAdForCompaniesRestController(JobAdService jobAdService, CompanyService companyService, AuthenticationHelper authenticationHelper, ModelMapper modelMapper) {
         this.jobAdService = jobAdService;
         this.companyService = companyService;
         this.authenticationHelper = authenticationHelper;
+        this.modelMapper = modelMapper;
     }
 
 
     @GetMapping
-    public List<JobAd> getAllJobAds(@RequestHeader HttpHeaders headers) {
+    public List<JobAdDtoOut> getAllJobAds(@RequestHeader HttpHeaders headers) {
         try{
             authenticationHelper.tryGetCompany(headers);
             List<JobAd> jobAdList = jobAdService.getAll();
 
-            return jobAdList;
+            return modelMapper.fromJobAdToJobAdDtoOutList(jobAdList);
         } catch (AuthorizationException e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
         }
     }
 
     @GetMapping("/{id}")
-    public JobAd getJobAdById(@RequestHeader HttpHeaders headers,
+    public JobAdDtoOut getJobAdById(@RequestHeader HttpHeaders headers,
                               @PathVariable int id) {
         try {
             authenticationHelper.tryGetCompany(headers);
 
-            return jobAdService.getJobAdById(id);
+            return modelMapper.fromJobAdToJobAdDtoOut(jobAdService.getJobAdById(id));
         } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         } catch (AuthorizationException e) {
