93719fa981dfe84e71fb47155c2bfb08db5b7e69
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-26 18:39:25 +0200

    impl register professional, get professional by username and id in ProfessionalService & ProfessionalRepository

--- JobMatch/src/main/java/com/project/jobmatch/services/ProfessionalServiceImpl.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/ProfessionalServiceImpl.java
@@ -1,16 +1,21 @@
 package com.project.jobmatch.services;
 
+import com.project.jobmatch.exceptions.EntityDuplicateException;
 import com.project.jobmatch.exceptions.EntityNotFoundException;
 import com.project.jobmatch.models.Professional;
 import com.project.jobmatch.repositories.interfaces.ProfessionalRepository;
 import com.project.jobmatch.services.interfaces.ProfessionalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProfessionalServiceImpl implements ProfessionalService {
 
     private final ProfessionalRepository professionalRepository;
 
+    @Autowired
     public ProfessionalServiceImpl(ProfessionalRepository professionalRepository) {
         this.professionalRepository = professionalRepository;
     }
@@ -19,13 +24,36 @@ public class ProfessionalServiceImpl implements ProfessionalService {
     public Professional getByUsername(String username) {
         return professionalRepository
                 .findProfessionalByUsername(username)
-                .orElseThrow(()->new EntityNotFoundException("Professional", username, "username"));
+                .orElseThrow(() -> new EntityNotFoundException("Professional", username, "username"));
     }
 
     @Override
     public Professional getProfessionalById(int id) {
         return professionalRepository
                 .findProfessionalById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Professional", id));
+                .orElseThrow(() -> new EntityNotFoundException("Professional", id));
+    }
+
+    @Override
+    public void registerProfessional(Professional professional) {
+
+        boolean existsDuplicate = true;
+
+        try {
+            getByUsername(professional.getUsername());
+        } catch (EntityNotFoundException e) {
+            existsDuplicate = false;
+        }
+
+        if (existsDuplicate) {
+            throw new EntityDuplicateException("Professional", "username", professional.getUsername());
+        } else {
+            professionalRepository.save(professional);
+        }
+    }
+
+    @Override
+    public List<Professional> getAllProfessionals() {
+        return professionalRepository.findAll();
     }
 }
--- JobMatch/src/main/java/com/project/jobmatch/services/interfaces/ProfessionalService.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/interfaces/ProfessionalService.java
@@ -2,8 +2,14 @@ package com.project.jobmatch.services.interfaces;
 
 import com.project.jobmatch.models.Professional;
 
+import java.util.List;
+
 public interface ProfessionalService {
     Professional getByUsername(String username);
 
     Professional getProfessionalById(int id);
+
+    void registerProfessional(Professional professional);
+
+    List<Professional> getAllProfessionals();
 }
