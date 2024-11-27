306ef5a48bc6c8e3875bcebf3646c7671ad0d976
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-27 11:15:23 +0200

    rearrange methods sequence in CompanyService & ProfessionalService

--- JobMatch/src/main/java/com/project/jobmatch/services/interfaces/ProfessionalService.java
+++ JobMatch/src/main/java/com/project/jobmatch/services/interfaces/ProfessionalService.java
@@ -6,15 +6,16 @@ import com.project.jobmatch.services.CloudinaryImage;
 import java.util.List;
 
 public interface ProfessionalService {
-    Professional getByUsername(String username);
-
-    Professional getProfessionalById(int id);
 
     void registerProfessional(Professional professional);
-
-    List<Professional> getAllProfessionals();
 
     void updateProfessional(Professional professionalAuthenticated, Professional professionalMapped);
 
     void uploadPictureToProfessional(Professional professionalAuthenticated, Professional professionalToUploadPicture, CloudinaryImage cloudinaryImage);
+
+    Professional getByUsername(String username);
+
+    Professional getProfessionalById(int id);
+
+    List<Professional> getAllProfessionals();
 }
