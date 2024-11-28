87686975d358126b1645db427ea76bf37e5ec01d
annavulcheva <anna.valcheva26@gmail.com>
2024-11-28 11:04:36 +0200

    feat: adding constructor for JobApplicationForCompaniesRestController

--- JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobApplicationForCompaniesRestController.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobApplicationForCompaniesRestController.java
@@ -1,5 +1,9 @@
 package com.project.jobmatch.controllers.rest.company;
 
+import com.project.jobmatch.helpers.AuthenticationHelper;
+import com.project.jobmatch.helpers.ModelMapper;
+import com.project.jobmatch.services.interfaces.JobAdService;
+import com.project.jobmatch.services.interfaces.JobApplicationService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/company-portal/job-applications")
 public class JobApplicationForCompaniesRestController {
 
+    private final JobApplicationService jobApplicationService;
+    private final JobAdService jobAdService;
+    private final AuthenticationHelper authenticationHelper;
+    private final ModelMapper modelMapper;
 
+    public JobApplicationForCompaniesRestController(JobApplicationService jobApplicationService,
+                                                    JobAdService jobAdService,
+                                                    AuthenticationHelper authenticationHelper,
+                                                    ModelMapper modelMapper) {
+        this.jobApplicationService = jobApplicationService;
+        this.jobAdService = jobAdService;
+        this.authenticationHelper = authenticationHelper;
+        this.modelMapper = modelMapper;
+    }
 }
