9fb1c4f9412db1f9da8e5f8af2313e0055945e93
annavulcheva <anna.valcheva26@gmail.com>
2024-11-28 11:06:41 +0200

    feat: adding getAllJobApplications method for JobApplicationForCompaniesRestController

--- JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobApplicationForCompaniesRestController.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/rest/company/JobApplicationForCompaniesRestController.java
@@ -2,10 +2,16 @@ package com.project.jobmatch.controllers.rest.company;
 
 import com.project.jobmatch.helpers.AuthenticationHelper;
 import com.project.jobmatch.helpers.ModelMapper;
+import com.project.jobmatch.models.dto.JobApplicationDtoOut;
 import com.project.jobmatch.services.interfaces.JobAdService;
 import com.project.jobmatch.services.interfaces.JobApplicationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpHeaders;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/company-portal/job-applications")
@@ -25,4 +31,10 @@ public class JobApplicationForCompaniesRestController {
         this.authenticationHelper = authenticationHelper;
         this.modelMapper = modelMapper;
     }
+
+    @GetMapping
+    public List<JobApplicationDtoOut> getAllJobApplications(@RequestHeader HttpHeaders headers) {
+        throw new UnsupportedOperationException();
+    }
+
 }
