fa354163c1205ee3c0c4a196f595ef106b3371ec
Nikolay Voynov <nikolay_voynov@abv.bg>
2024-11-29 16:33:15 +0200

    test: impl tests for getJobAdById() in JobAdService

--- JobMatch/src/test/java/com/project/jobmatch/services/JobAdServiceImplTests.java
+++ JobMatch/src/test/java/com/project/jobmatch/services/JobAdServiceImplTests.java
@@ -1,4 +1,48 @@
 package com.project.jobmatch.services;
 
+import com.project.jobmatch.exceptions.EntityNotFoundException;
+import com.project.jobmatch.models.JobAd;
+import com.project.jobmatch.repositories.interfaces.JobAdRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static com.project.jobmatch.Helpers.createMockJobAd;
+
+@ExtendWith(MockitoExtension.class)
 public class JobAdServiceImplTests {
+
+    @Mock
+    JobAdRepository mockJobAdRepository;
+
+    @InjectMocks
+    JobAdServiceImpl mockJobAdService;
+
+    @Test
+    public void getJobAdById_Should_ReturnJobAd_When_IdExists() {
+        // Arrange
+        JobAd mockJobAd = createMockJobAd();
+
+        Mockito.when(mockJobAdRepository.findJobAdById(Mockito.anyInt()))
+                .thenReturn(Optional.of(mockJobAd));
+
+        // Act
+        JobAd jobAd = mockJobAdService.getJobAdById(Mockito.anyInt());
+
+        // Assert
+        Assertions.assertEquals(mockJobAd, jobAd);
+    }
+
+    @Test
+    public void getJobAdById_Should_ThrowException_When_IdDoesNotExist() {
+        //Act, Assert
+        Assertions.assertThrows(EntityNotFoundException.class,
+                () -> mockJobAdService.getJobAdById(Mockito.anyInt()));
+    }
 }
