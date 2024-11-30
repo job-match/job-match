1b407760b247b457bd7056f6ec967b2f50251b33
annavulcheva <anna.valcheva26@gmail.com>
2024-11-30 10:32:05 +0200

    test: setting up test class for LocationServiceImpl

--- JobMatch/src/test/java/com/project/jobmatch/services/LocationServiceImplTests.java
+++ JobMatch/src/test/java/com/project/jobmatch/services/LocationServiceImplTests.java
@@ -1,4 +1,16 @@
 package com.project.jobmatch.services;
 
+import com.project.jobmatch.repositories.interfaces.LocationRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class LocationServiceImplTests {
+    @Mock
+    private LocationRepository mockLocationRepository;
+
+    @InjectMocks
+    LocationServiceImpl mockLocationService;
 }
