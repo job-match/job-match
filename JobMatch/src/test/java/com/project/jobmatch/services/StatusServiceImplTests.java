73153e5591cc36ac4c9deb0c067a0fc77a7d2a2f
annavulcheva <anna.valcheva26@gmail.com>
2024-11-30 10:44:57 +0200

    test: setting up test class for StatusServiceImpl

--- JobMatch/src/test/java/com/project/jobmatch/services/StatusServiceImplTests.java
+++ JobMatch/src/test/java/com/project/jobmatch/services/StatusServiceImplTests.java
@@ -1,4 +1,16 @@
 package com.project.jobmatch.services;
 
+import com.project.jobmatch.repositories.interfaces.StatusRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class StatusServiceImplTests {
+    @Mock
+    private StatusRepository mockStatusRepository;
+
+    @InjectMocks
+    private StatusServiceImpl mockStatusService;
 }
