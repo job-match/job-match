5fc0619782e5b571cdea7d7036349995ceceb6c3
Owner <kirilovmail90@gmail.com>
2024-12-03 13:05:37 +0200

    feat: added HomeMvcController.java and header in index.html

---
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/mvc/HomeMvcController.java
@@ -0,0 +1,12 @@
+package com.project.jobmatch.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeMvcController {
+
+    public HomeMvcController() {
+    }
+}
