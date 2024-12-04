1939c7ebc60ab883aa11dfd73141e5b741189b4c
annavulcheva <anna.valcheva26@gmail.com>
2024-12-04 11:05:53 +0200

    feat: implementing showLoginPage and handleLogin methods in the AuthenticationMvcControllerForProfessional class

--- JobMatch/src/main/java/com/project/jobmatch/controllers/mvc/professional/AuthenticationMvcControllerForProfessional.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/mvc/professional/AuthenticationMvcControllerForProfessional.java
@@ -1,4 +1,59 @@
 package com.project.jobmatch.controllers.mvc.professional;
 
+import com.project.jobmatch.exceptions.AuthorizationException;
+import com.project.jobmatch.helpers.AuthenticationHelper;
+import com.project.jobmatch.helpers.ModelMapper;
+import com.project.jobmatch.models.dto.ProfessionalLoginDto;
+import com.project.jobmatch.services.interfaces.ProfessionalService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
 public class AuthenticationMvcControllerForProfessional {
+    private final ProfessionalService professionalService;
+    private final AuthenticationHelper authenticationHelper;
+    private final ModelMapper modelMapper;
+
+    public AuthenticationMvcControllerForProfessional(ProfessionalService professionalService, AuthenticationHelper authenticationHelper, ModelMapper modelMapper) {
+        this.professionalService = professionalService;
+        this.authenticationHelper = authenticationHelper;
+        this.modelMapper = modelMapper;
+    }
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+
+    @GetMapping("/professional/login")
+    public String showLoginPage(Model model) {
+        model.addAttribute("login", new ProfessionalLoginDto());
+        return "login-professional";
+    }
+
+    @PostMapping("/professional/login")
+    public String handleLogin(@Valid @ModelAttribute("login") ProfessionalLoginDto professionalLoginDto,
+                              BindingResult bindingResult,
+                              HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            return "login-professional";
+        }
+
+        try {
+            authenticationHelper.verifyAuthenticationProfessional(professionalLoginDto.getUsername(), professionalLoginDto.getPassword());
+            session.setAttribute("currentUser", professionalLoginDto.getUsername());
+            return "redirect:/";
+        } catch (AuthorizationException e) {
+            bindingResult.rejectValue("username", "auth_error", e.getMessage());
+            return "login";
+        }
+    }
 }
