75d0b4d1b691aa4ac70e0a18ee2ab3e9e4154478
annavulcheva <anna.valcheva26@gmail.com>
2024-12-05 15:34:25 +0200

    feat: created a new register dto for professionals

---
+++ JobMatch/src/main/java/com/project/jobmatch/models/dto/ProfessionalRegisterDto.java
@@ -0,0 +1,42 @@
+package com.project.jobmatch.models.dto;
+
+public class ProfessionalRegisterDto extends ProfessionalLoginDto{
+    private String passwordConfirmation;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public ProfessionalRegisterDto() {}
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
