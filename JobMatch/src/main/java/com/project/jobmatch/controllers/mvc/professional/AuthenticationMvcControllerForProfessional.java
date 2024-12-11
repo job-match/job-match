c7164440c8273a8c71a7078502d1fe668ba16a87
annavulcheva <anna.valcheva26@gmail.com>
2024-12-11 12:13:44 +0200

    refactor: modified handleLogin and handleRegister methods

--- JobMatch/src/main/java/com/project/jobmatch/controllers/mvc/professional/AuthenticationMvcControllerForProfessional.java
+++ JobMatch/src/main/java/com/project/jobmatch/controllers/mvc/professional/AuthenticationMvcControllerForProfessional.java
@@ -1,8 +1,10 @@
 package com.project.jobmatch.controllers.mvc.professional;
 
 import com.project.jobmatch.exceptions.AuthorizationException;
+import com.project.jobmatch.exceptions.EntityDuplicateException;
 import com.project.jobmatch.helpers.AuthenticationHelper;
 import com.project.jobmatch.helpers.ModelMapper;
+import com.project.jobmatch.models.Professional;
 import com.project.jobmatch.models.dto.ProfessionalLoginDto;
 import com.project.jobmatch.models.dto.ProfessionalRegisterDto;
 import com.project.jobmatch.services.interfaces.ProfessionalService;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.project.jobmatch.helpers.MvcControllersConstants.PASSWORD_CONFIRMATION_ERROR_MESSAGE;
 
 @Controller
 @RequestMapping("/auth")
@@ -34,17 +38,26 @@ public class AuthenticationMvcControllerForProfessional {
         return session.getAttribute("currentUser") != null;
     }
 
-    @GetMapping("/professional/login")
+    @GetMapping("/professional-portal/login")
     public String showLoginPage(Model model) {
-        model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+        if (!model.containsAttribute("loginProfessional")) {
+            model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+        }
+        if (!model.containsAttribute("registerProfessional")) {
+            model.addAttribute("registerProfessional", new ProfessionalRegisterDto());
+        }
         return "professional/login-professional";
     }
 
-    @PostMapping("/professional/login")
+    @PostMapping("/professional-portal/login")
     public String handleLogin(@Valid @ModelAttribute("loginProfessional") ProfessionalLoginDto professionalLoginDto,
                               BindingResult bindingResult,
-                              HttpSession session) {
+                              HttpSession session,
+                              Model model) {
         if (bindingResult.hasErrors()) {
+            if (!model.containsAttribute("registerProfessional")) {
+                model.addAttribute("registerProfessional", new ProfessionalRegisterDto());
+            }
             return "professional/login-professional";
         }
 
@@ -54,35 +67,53 @@ public class AuthenticationMvcControllerForProfessional {
             return "redirect:/";
         } catch (AuthorizationException e) {
             bindingResult.rejectValue("username", "auth_error", e.getMessage());
+            if (!model.containsAttribute("registerProfessional")) {
+                model.addAttribute("registerProfessional", new ProfessionalRegisterDto());
+            }
             return "professional/login-professional";
         }
     }
 
-    @GetMapping("/professional/register")
+    @GetMapping("/professional-portal/register")
     public String showRegisterPage(Model model) {
-        model.addAttribute("registerProfessional", new ProfessionalRegisterDto());
+        if (!model.containsAttribute("loginProfessional")) {
+            model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+        }
+        if (!model.containsAttribute("registerProfessional")) {
+            model.addAttribute("registerProfessional", new ProfessionalRegisterDto());
+        }
         return "professional/login-professional";
     }
 
-//    @PostMapping("professional//register")
-//    public String handleRegister(@Valid @ModelAttribute("register") ProfessionalRegisterDto professionalRegisterDto,
-//                                 BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return "professional/login-professional";
-//        }
-//
-//        if (!professionalRegisterDto.getPassword().equals(professionalRegisterDto.getPasswordConfirmation())) {
-//            bindingResult.rejectValue("passwordConfirm", "password_error", PASSWORD_CONFIRMATION_ERROR_MESSAGE);
-//            return "professional/login-professional";
-//        }
-//
-//        try {
-//            Professional professional = modelMapper.fromProfessionalDtoInToProfessional(professionalRegisterDto);
-//            userService.register(user);
-//            return "redirect:/auth/login";
-//        } catch (EntityDuplicateException e) {
-//            bindingResult.rejectValue("username", "username_error", e.getMessage());
-//            return "professional/login-professional";
-//        }
-//    }
+    @PostMapping("/professional-portal/register")
+    public String handleRegister(@Valid @ModelAttribute("registerProfessional") ProfessionalRegisterDto professionalRegisterDto,
+                                 BindingResult bindingResult,
+                                 Model model) {
+        if (bindingResult.hasErrors()) {
+            if (!model.containsAttribute("loginProfessional")) {
+                model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+            }
+            return "professional/login-professional";
+        }
+
+        if (!professionalRegisterDto.getPassword().equals(professionalRegisterDto.getPasswordConfirmation())) {
+            bindingResult.rejectValue("passwordConfirm", "password_error", PASSWORD_CONFIRMATION_ERROR_MESSAGE);
+            if (!model.containsAttribute("loginProfessional")) {
+                model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+            }
+            return "professional/login-professional";
+        }
+
+        try {
+            Professional professional = modelMapper.fromProfessionalRegisterDto(professionalRegisterDto);
+            professionalService.registerProfessional(professional);
+            return "redirect:/auth/professional-portal/login";
+        } catch (EntityDuplicateException e) {
+            bindingResult.rejectValue("username", "username_error", e.getMessage());
+            if (!model.containsAttribute("loginProfessional")) {
+                model.addAttribute("loginProfessional", new ProfessionalLoginDto());
+            }
+            return "professional/login-professional";
+        }
+    }
 }
