5e29fd53f3e0c3772b9a48a89e4ff859c0daa1c4
annavulcheva <anna.valcheva26@gmail.com>
2024-12-13 10:50:25 +0200

    feat: implemented CompanyLoginDto class

--- JobMatch/src/main/java/com/project/jobmatch/models/dto/CompanyLoginDto.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/dto/CompanyLoginDto.java
@@ -1,4 +1,25 @@
 package com.project.jobmatch.models.dto;
 
 public class CompanyLoginDto {
+    private String username;
+    private String password;
+
+    public CompanyLoginDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
