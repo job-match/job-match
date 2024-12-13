73f29c204b019ab3566043f9939f9b98eea431c8
annavulcheva <anna.valcheva26@gmail.com>
2024-12-13 10:56:54 +0200

    feat: implemented CompanyRegisterDto class

--- JobMatch/src/main/java/com/project/jobmatch/models/dto/CompanyRegisterDto.java
+++ JobMatch/src/main/java/com/project/jobmatch/models/dto/CompanyRegisterDto.java
@@ -1,4 +1,41 @@
 package com.project.jobmatch.models.dto;
 
-public class CompanyRegisterDto {
+import jakarta.validation.constraints.NotEmpty;
+
+public class CompanyRegisterDto extends CompanyLoginDto {
+    @NotEmpty(message = "Cannot be empty!")
+    private String passwordConfirmation;
+
+    @NotEmpty(message = "Cannot be empty!")
+    private String name;
+
+    @NotEmpty(message = "Cannot be empty!")
+    private String email;
+
+    public CompanyRegisterDto() {
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
